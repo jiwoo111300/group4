b423c150a00c07767ef553e5bbf6244cb3fffc08
박지우 <jiwoo111300@gmail.com>
2025-10-31 10:22:26 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('4모둠의 유리함수 교과서')
+st.write('김여은, 김은채, 박지우, 이세영, 편유빈')
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #bff2ff;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
